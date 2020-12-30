c57b651e4ce17f7f938d639fcbb289370061cea3
Greg Vargas <gregory.vargas18@gmail.com>
2020-12-30 17:38:24 -0600

    Create screen for adding a new todo list item

--- ScrubList/Controllers/NewListItemScreen.swift
+++ ScrubList/Controllers/NewListItemScreen.swift
@@ -8,7 +8,43 @@
 import UIKit
 
 class NewListItemScreen: UIViewController {
+    @IBOutlet weak var newItemNameTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.hideKeyboardWhenTappedAround()
+        
+        newItemNameTextField.becomeFirstResponder()
+        newItemNameTextField.delegate = self
+    }
+    
+    @IBAction func saveButtonPressed(_ sender: UIBarButtonItem) {
+        if let newItemName = newItemNameTextField.text, !newItemName.isEmpty {
+            print(newItemName)
+            newItemNameTextField.text = ""
+            newItemNameTextField.resignFirstResponder()
+        } else {
+            print("Type something")
+        }
+    }
+}
+
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
+
+extension NewListItemScreen: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        newItemNameTextField.resignFirstResponder()
+        
+        return true
     }
 }
