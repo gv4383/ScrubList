44c52de7fed25f4c8857d8a097ad246bcce0cea7
gv4383 <gregory.vargas18@gmail.com>
2022-08-17 18:51:02 -0500

    Create task model and set up table view

---
+++ ScrubList/Models/Task.swift
@@ -0,0 +1,13 @@
+//
+//  Task.swift
+//  ScrubList
+//
+//  Created by Goyo Vargas on 8/17/22.
+//
+
+import Foundation
+
+struct Task {
+    let id = UUID()
+    let name: String
+}
--- ScrubList/Screens/TasksListViewController.swift
+++ ScrubList/Screens/TasksListViewController.swift
@@ -8,14 +8,50 @@
 import UIKit
 
 class TasksListViewController: UIViewController {
+    let tableView = UITableView()
+    
+    var tasks: [Task] = [
+        Task(name: "Task 1"),
+        Task(name: "Task 2"),
+        Task(name: "Task 3")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        configure()
+        configureVC()
+        configureTableView()
     }
     
-    private func configure() {
-        view.backgroundColor = .systemPurple
+    private func configureVC() {
+        view.backgroundColor = .systemBackground
         title = "Tasks"
+    }
+    
+    private func configureTableView() {
+        view.addSubview(tableView)
+        
+        tableView.frame = view.bounds
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+}
+
+extension TasksListViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let task = tasks[indexPath.row]
+        let cell = UITableViewCell()
+        
+        cell.textLabel?.text = task.name
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
